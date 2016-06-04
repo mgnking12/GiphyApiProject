5f79b45fc7d37deb939bc686b10d42b30d0c97d3
Megan King <mgnking12@gmail.com>
2016-06-04 18:51:07 -0500

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ var gifButtons="";
 var gifBArray= ["Michael Scott","Dwight Schrute","Jim Halpert","Pam Beesly","Ryan Howard","Andy Bernard","Angela Martin","Kelly Kapoor","Oscar Martinez","Darryl Philbin","Erin Hannon","Toby Flenderson","Kevin Malone","Phyllis Lapin","Stanley Hudson","Meredith Palmer","Creed Bratton","Nellie Bertram"];
 var giphyKey = "dc6zaTOxFJmzC";
 var gInput = "";
-
+var rating = "";
 function renderButtons(){
 	for (var i = 0; i < gifBArray.length; i++) {
 		gifButtons += "<button class='btn gButtonStyle gif-buttons' id='arrayButton' data-name='" + gifBArray[i] +"'>" + gifBArray[i] + "</button>";
@@ -35,15 +35,35 @@ $('body').on('click', '.gif-buttons', function(event){
 	 .done(function(response) {
 	 	console.log(response);
 	 	var results = response.data;
+	 	for (var i = 0; i < response.length; i++){
+	 	var rating = $('<p>').text("Rating: " + response[i].rating);
+	 }
+
 	 	for(var i=0; i < results.length; i++){
-	 	var gifImage = $('<img>');
-	 	gifImage.attr('src', results[i].images.fixed_height.url);
-	 	$('#gifsAppear').prepend(gifImage);    
+	 		console.log(rating);
+	 	var gifImage = $('<img src=' + results[i].images.fixed_height_still.url + ' data-still=' +
+                	results[i].images.fixed_height_still.url + ' data-animate=' +
+                	results[i].images.fixed_height.url + ' data-state="still" class="gifImage">' + rating);
+	 	$('#gifsAppear').prepend(gifImage);   
+
 	 }
 	});
 	 ;
 });
+$('body').on('click', '.gifImage', function(event){
 
+
+     var imgState = $(this).attr('data-state');
+	    if(imgState == 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }
+        else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+});
+	
 });
 
 
